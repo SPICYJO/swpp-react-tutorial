b6a74e97d4f21a38e01d201ac490029a3a192c99
Seungwoo Jo <sjosjo1204@gmail.com>
2020-09-18 18:20:47 +0900

    Finish practice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoDetail from './components/TodoDetail/TodoDetail';
 import TodoList from './containers/TodoList/TodoList';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import NewTodo from './containers/NewTodo/NewTodo';
@@ -11,15 +12,10 @@ function App() {
 		<div className='App'>
 			<Switch>
 				<Route path='/todos' exact render={() => <TodoList title="My TODOs!" />} />
-<<<<<<< HEAD
 				<Route path='/todos/:id' exact component={TodoDetail}/> 
 				<Route path='/new-todo' exact component={NewTodo} />
 				<Redirect exact from='/' to='/todos' />
 				<Route render={() => <h1>Not Found</h1>} />
-=======
-				<Route path='/new-todo' exact component={NewTodo} />
-				<Redirect exact from='/' to='/todos' />
->>>>>>> 1deb1a11c8439a19b74f1aede30bbc917ffd583c
 			</Switch>
 		</div>
 	</BrowserRouter>
